6921c870170dda67ecc4cba2ba6615643e989f98
Remi Cakir <remi.cakir@hxperience.com>
2021-07-26 14:29:46 +0200

    add widget route

--- routes/api/api.routes.js
+++ routes/api/api.routes.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const csv = require("csvtojson");
 const express = require('express');
 const apiRouter = express.Router();
+const moment = require('moment')
 
 let config = {
   headers: {
@@ -10,6 +11,9 @@ let config = {
     "Content-Type": "application/vnd.flux"
   }
 }
+
+const defaultStart = moment('25072021', 'DDMMYYYY').unix();
+let startValue;
 
 /* Configuration */
 class ApiRouterClass {
@@ -23,87 +27,164 @@ class ApiRouterClass {
     |> filter(fn: (r) => r["nodeID"] == "${nodeId}")`
   };
 
+  getWidgetData(arr) {
+    let arrValue = arr.map(x => Number(x._value));
+    return {
+      min: Math.min(...arrValue),
+      max: Math.max(...arrValue),
+      mean: arrValue.reduce((a, b) => a + b) / arrValue.length,
+      last: arrValue[arrValue.length - 1]
+    }
+  }
+
+  getStartValue(isWidget) {
+    return isWidget ? moment().startOf('day').add(2, 'hours').unix() : defaultStart;
+  }
+
   routes() {
     apiRouter.get(`/temperature`, (req, res) => {
-      axios.post(process.env.URL, this.getPostData('1627207493', 'Temperature', 'global'), config)
+      axios.post(process.env.URL, this.getPostData(this.getStartValue(req.query.widget), 'Temperature', 'global'), config)
         .then(response => {
           csv().fromString(response.data)
-            .then((data) => res.json({
-              data: data.map(x => ({ time: x._time, value: x._value })),
-              unit: '°C'
-            }))
+            .then((data) => {
+              if (req.query.widget) {
+                res.json({
+                  data: this.getWidgetData(data),
+                  unit: '°C'
+                })
+              } else {
+                res.json({
+                  data: data.map(x => ({ time: x._time, value: x._value })),
+                  unit: '°C'
+                })
+              }
+            })
         })
         .catch(error => res.json(error));
     });
 
     apiRouter.get(`/luminosity`, (req, res) => {
-      axios.post(process.env.URL, this.getPostData('1627207493', 'Luminosite', 'global'), config)
+      axios.post(process.env.URL, this.getPostData(this.getStartValue(req.query.widget), 'Luminosite', 'global'), config)
         .then(response => {
           csv().fromString(response.data)
-            .then((data) => res.json({
-              data: data.map(x => ({ time: x._time, value: x._value })),
-              unit: 'lux'
-            }))
+            .then((data) => {
+              if (req.query.widget) {
+                res.json({
+                  data: this.getWidgetData(data),
+                  unit: 'lux'
+                })
+              } else {
+                res.json({
+                  data: data.map(x => ({ time: x._time, value: x._value })),
+                  unit: 'lux'
+                })
+              }
+            })
         })
         .catch(error => res.json(error));
     });
 
     apiRouter.get(`/humidity/air`, (req, res) => {
-      axios.post(process.env.URL, this.getPostData('1627207493', 'Humidite', 'global'), config)
+      axios.post(process.env.URL, this.getPostData(this.getStartValue(req.query.widget), 'Humidite', 'global'), config)
         .then(response => {
           csv().fromString(response.data)
-            .then((data) => res.json({
-              data: data.map(x => ({ time: x._time, value: x._value })),
-              unit: '%'
-            }))
+            .then((data) => {
+              if (req.query.widget) {
+                res.json({
+                  data: this.getWidgetData(data),
+                  unit: '%'
+                })
+              } else {
+                res.json({
+                  data: data.map(x => ({ time: x._time, value: x._value })),
+                  unit: '%'
+                })
+              }
+            })
         })
         .catch(error => res.json(error));
     });
 
     apiRouter.get(`/humidity/basilic`, (req, res) => {
-      axios.post(process.env.URL, this.getPostData('1627207493', 'Humidite', 'basilic'), config)
+      axios.post(process.env.URL, this.getPostData(this.getStartValue(req.query.widget), 'Humidite', 'basilic'), config)
         .then(response => {
           csv().fromString(response.data)
-            .then((data) => res.json({
-              data: data.map(x => ({ time: x._time, value: x._value })),
-              unit: '%'
-            }))
+            .then((data) => {
+              if (req.query.widget) {
+                res.json({
+                  data: this.getWidgetData(data),
+                  unit: '%'
+                })
+              } else {
+                res.json({
+                  data: data.map(x => ({ time: x._time, value: x._value })),
+                  unit: '%'
+                })
+              }
+            })
         })
         .catch(error => res.json(error));
     });
 
     apiRouter.get(`/humidity/menthe`, (req, res) => {
-      axios.post(process.env.URL, this.getPostData('1627207493', 'Humidite', 'menthe'), config)
+      axios.post(process.env.URL, this.getPostData(this.getStartValue(req.query.widget), 'Humidite', 'menthe'), config)
         .then(response => {
           csv().fromString(response.data)
-            .then((data) => res.json({
-              data: data.map(x => ({ time: x._time, value: x._value })),
-              unit: '%'
-            }))
+            .then((data) => {
+              if (req.query.widget) {
+                res.json({
+                  data: this.getWidgetData(data),
+                  unit: '%'
+                })
+              } else {
+                res.json({
+                  data: data.map(x => ({ time: x._time, value: x._value })),
+                  unit: '%'
+                })
+              }
+            })
         })
         .catch(error => res.json(error));
     });
 
     apiRouter.get(`/humidity/poivron`, (req, res) => {
-      axios.post(process.env.URL, this.getPostData('1627207493', 'Humidite', 'poivron'), config)
+      axios.post(process.env.URL, this.getPostData(this.getStartValue(req.query.widget), 'Humidite', 'poivron'), config)
         .then(response => {
           csv().fromString(response.data)
-            .then((data) => res.json({
-              data: data.map(x => ({ time: x._time, value: x._value })),
-              unit: '%'
-            }))
+            .then((data) => {
+              if (req.query.widget) {
+                res.json({
+                  data: this.getWidgetData(data),
+                  unit: '%'
+                })
+              } else {
+                res.json({
+                  data: data.map(x => ({ time: x._time, value: x._value })),
+                  unit: '%'
+                })
+              }
+            })
         })
         .catch(error => res.json(error));
     });
 
     apiRouter.get(`/humidity/tomate`, (req, res) => {
-      axios.post(process.env.URL, this.getPostData('1627207493', 'Humidite', 'tomate'), config)
+      axios.post(process.env.URL, this.getPostData(this.getStartValue(req.query.widget), 'Humidite', 'tomate'), config)
         .then(response => {
           csv().fromString(response.data)
-            .then((data) => res.json({
-              data: data.map(x => ({ time: x._time, value: x._value })),
-              unit: '%'
-            }))
+            .then((data) => {
+              if (req.query.widget) {
+                res.json({
+                  data: this.getWidgetData(data),
+                  unit: '%'
+                })
+              } else {
+                res.json({
+                  data: data.map(x => ({ time: x._time, value: x._value })),
+                  unit: '%'
+                })
+              }
+            })
         })
         .catch(error => res.json(error));
     });
