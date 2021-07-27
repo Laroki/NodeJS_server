3853b1cd946a0f61b627da980cc702f615d9081f
Remi Cakir <remi.cakir@hxperience.com>
2021-07-27 18:14:28 +0200

    add https

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 /* imports */
 require('dotenv').config(); // Variable d'environnement 
 const express = require('express') // module important config du serv.
-
+var fs = require('fs')
+var https = require('https')
 //Inner
 const { mainRouter } = require(`./routes/main.router`);
 
@@ -11,6 +12,14 @@ const port = process.env.PORT;
 server.use(`/`, mainRouter);
 
 /* Lancer le serveur */
-server.listen(port, () => {
+// server.listen(port, () => {
+//   console.log(`Server listening on port ${port}.`)
+// });
+
+https.createServer({
+  key: fs.readFileSync('./server.key'),
+  cert: fs.readFileSync('./server.cert')
+}, server)
+.listen(port, function () {
   console.log(`Server listening on port ${port}.`)
-});
+})
